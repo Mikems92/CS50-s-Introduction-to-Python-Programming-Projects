4059ea3df35e398c0e9c9a4cffc686986a30a2de
Mikems92 <Mikems92@users.noreply.github.com>
2024-01-16 00:13:32 +0100

    automated commit by check50 [check50=True]

--- meal.py
+++ meal.py
@@ -1,20 +1,19 @@
 def main():
-    time_to_eat = convert(input("What time is it ? "))
-    print(time_to_eat)
+    y = convert(input("What time is it ? "))
+    if 7 <= y <= 8 :
+        print("It's breakfast time")
+    elif 12 <= y <= 13 :
+        print("It's lunch time")
+    elif 18 <= y <= 19 :
+        print("It's dinner time")
+    else :
+        print("")
 
 
 def convert(time):
     hours , minutes = time.split (":")
     x = int(hours) + int(minutes)/60
-
-    if 7 <= x <= 8 :
-        return "It's breakfast time"
-    elif 12 <= x <= 13 :
-        return "It's lunch time"
-    elif 18 <= x <= 19 :
-        return "It's dinner time"
-    else :
-        return ""
+    return x
 
 
 if __name__ == "__main__":
