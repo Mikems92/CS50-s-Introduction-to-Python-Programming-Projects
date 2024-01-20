5b6c657da4a4bcd072f0b8e7de79bce527df9cdb
Mikems92 <Mikems92@users.noreply.github.com>
2024-01-20 18:44:41 +0100

    automated commit by check50 [check50=True]

--- outdated.py
+++ outdated.py
@@ -22,11 +22,10 @@ while True:
         if month in months :
             month = months.index(month) + 1
     try:
-        if "/" and "," not in date:
-            if int(month) > 12 or int(day) > 31 :
-                True
-            else:
-                break
+        if int(month) > 12 or int(day) > 31 :
+            True
+        else:
+            break
     except ValueError :
         continue
     else :
