cf662aedd3f0f7d3b7f4b27ce0f504d7559b42ca
Mikems92 <Mikems92@users.noreply.github.com>
2024-01-20 23:32:51 +0100

    automated commit by check50 [check50=True]

--- figlet.py
+++ figlet.py
@@ -8,13 +8,15 @@ if len(sys.argv) == 1:
     random_font = random.choice(figlet.getFonts())
     figlet.setFont(font=random_font)
     print(figlet.renderText(input))
-elif len(sys.argv) == 3:
-
-        if sys.argv[1] == "-f" or sys.argv[1] == "--font" and sys.argv[2] in figlet.getFonts():
-            figlet.setFont(font=sys.argv[2])
-            print(figlet.renderText(input))
-        elif sys.argv[1] != "-f" or sys.argv[1] != "--font" and sys.argv[2] not in figlet.getFonts():
-            sys.exit("Invalid usage")
 elif len(sys.argv) == 2:
     sys.exit("Invalid usage")
+elif len(sys.argv) == 3:
+        if sys.argv[1] == "-f" or sys.argv[1] == "--font" and sys.argv[2] not in figlet.getFonts():
+            sys.exit("Invalid usage")
+        elif sys.argv[1] == "-f" or sys.argv[1] == "--font" and sys.argv[2] in figlet.getFonts():
+            figlet.setFont(font=sys.argv[2])
+            print(figlet.renderText(input))
+        elif sys.argv[1] != "-f" or sys.argv[1] != "--font" or sys.argv[2] not in figlet.getFonts():
+            sys.exit("Invalid usage")
+
 
