482879dea77ae3b9540897d30c2b7b00754529cf
Mikems92 <Mikems92@users.noreply.github.com>
2024-01-20 23:55:44 +0100

    automated commit by check50 [check50=True]

--- figlet.py
+++ figlet.py
@@ -2,12 +2,12 @@ import random
 import sys
 from pyfiglet import Figlet
 
-fonts = figlet.getFonts()
 figlet = Figlet()
+fonts = figlet.getFonts()
 if len(sys.argv) == 1:
     figlet.setFont(font=random.choice(fonts))
 elif len(sys.argv) == 3 and (sys.argv[1] == "-f" or sys.argv[1] == "--font") and sys.argv[2] in fonts() :
-        figlet.setFont(font=sys.argv[2])
+    figlet.setFont(font=sys.argv[2])
 else :
     sys.exit("Invalid usage")
 
