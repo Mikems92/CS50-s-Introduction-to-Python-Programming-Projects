ed224f658dcd20f18a423ce44a963b076b6c26ff
Mikems92 <Mikems92@users.noreply.github.com>
2024-01-24 21:04:17 +0100

    automated commit by check50 [check50=True]

--- test_bank.py
+++ test_bank.py
@@ -2,13 +2,13 @@ from bank import value
 
 
 def test_hello():
-    assert value ("hello, world") == f"${0}"
-    assert value ("Hello, world") == f"${0}"
+    assert value ("hello, world") == 0
+    assert value ("Hello, world") == 0
 
 def test_h():
-    assert value ("how are you ?") == f"${20}"
+    assert value ("how are you ?") == 20
 
 def test_else():
-    assert value ("What are you doing ?") == f"${100}"
+    assert value ("What are you doing ?") == 100
 
 
