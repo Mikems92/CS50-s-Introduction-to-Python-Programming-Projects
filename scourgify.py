e745cb6abf74d3d91f49ec198347a8b3ba41d54c
Mikems92 <Mikems92@users.noreply.github.com>
2024-01-27 02:41:26 +0100

    automated commit by check50 [check50=True]

--- scourgify.py
+++ scourgify.py
@@ -23,16 +23,20 @@ else:
 fl = []
 flh = []
 with open(sys.argv[1]) as file:
-    reader = csv.reader(file)
-    for name, house in reader:
-        last_first = name.strip('"')
-        fl.append(last_first)
-    fl.pop(0)
-with open(sys.argv[2], "w", newline = '') as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        last, first = row["name"].split(",")
+        house = row["house"]
+        fl.append({"last": last.strip(), "first":first.strip(), "house":house.strip()})
+
+with open(sys.argv[2], "a") as file:
     dw = csv.DictWriter(file, fieldnames=["first", "last", "house"])
     dw.writeheader()
-    for i in fl:
-        last, first = i.split(", ")
+    for output in fl:
+        last = output["last"]
+        first = output["first"]
+        house = output["house"]
         dw.writerow({"first":first.strip(), "last":last.strip(), "house":house.strip()})
+
 
 
