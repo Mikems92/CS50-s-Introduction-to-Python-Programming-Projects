247ec0d45eb20fa7331afbc672cd006622725d6e
Mikems92 <Mikems92@users.noreply.github.com>
2024-01-28 23:09:07 +0100

    automated commit by check50 [check50=True]

--- numb3rs.py
+++ numb3rs.py
@@ -6,7 +6,7 @@ def main():
 
 
 def validate(ip):
-    if re.search(r"^([0-9]{1,3}\.){3}[0-9]{1,3}$", ip.strip()) :
+    if re.search(r"^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$", ip.strip()) :
         bytes = ip.split(".")
         for byte in bytes:
             if int(byte) < 0 or int(byte) > 255:
