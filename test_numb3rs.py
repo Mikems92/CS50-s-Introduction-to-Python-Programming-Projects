49827c3394634eae6086f234b0422c932b1df163
Mikems92 <Mikems92@users.noreply.github.com>
2024-01-28 23:09:50 +0100

    automated commit by check50 [check50=True]

--- test_numb3rs.py
+++ test_numb3rs.py
@@ -2,10 +2,10 @@ from numb3rs import validate
 
 
 def test_check_format():
-    assert validate("1.2.3.4") == True
-    assert validate("1.2.3.4.5") == False
+    assert validate(r"1.2.3.4") == True
+    assert validate(r"1.2.3.4.5") == False
 
 def test_check_number():
-    assert validate("255.0.78.100") == True
-    assert validate("275.2.3.4") == False
-    assert validate("1.2555.1.1") == False
+    assert validate(r"255.0.78.100") == True
+    assert validate(r"275.2.3.4") == False
+    assert validate(r"1.2555.1.1") == False
