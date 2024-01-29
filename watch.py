c64553798f276eadd768fd383d2efd33c9b1bc94
Mikems92 <Mikems92@users.noreply.github.com>
2024-01-29 23:27:37 +0100

    automated commit by check50 [check50=True]

--- watch.py
+++ watch.py
@@ -7,8 +7,7 @@ def main():
 
 
 def parse(s):
-    output = re.search (r'^(.*)http(s)?://(www\.)?youtube\.com/embed/([a-zA-Z0-9]+)"(.*)$', s)
-    if output :
+    if output := re.search (r'^(.*)http(s)?://(www\.)?youtube\.com/embed/([a-zA-Z0-9]+)"(.*)$', s)
         return("https://youtu.be/" + output.group(4))
     return(f"None")
 
