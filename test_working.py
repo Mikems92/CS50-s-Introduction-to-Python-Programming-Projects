9be7f61e66681ba2840de9ab39fdc397e7a13d6a
Mikems92 <Mikems92@users.noreply.github.com>
2024-01-31 00:52:30 +0100

    automated commit by check50 [check50=True]

--- test_working.py
+++ test_working.py
@@ -7,7 +7,7 @@ def test_convert():
     assert convert("10 PM to 8 AM") == "22:00 to 08:00"
     assert convert("10:30 PM to 8:50 AM") == "22:30 to 08:50"
     with pytest.raises (ValueError) :
-        convert("9:60 AM to 5:60 PM")
-        convert("9 AM  5 PM")
+        convert("9:60 AM 5:60 PM")
+        convert("9 AM - 5 PM")
         convert("25:61 AM - 25:61 PM")
 
