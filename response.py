30d9f3a6777db8ce7e14b36cc6e7244eb119143f
Mikems92 <Mikems92@users.noreply.github.com>
2024-01-31 23:14:42 +0100

    automated commit by check50 [check50=True]

--- response.py
+++ response.py
@@ -1,18 +1,14 @@
-import re
 from validator_collection import validators, checkers, errors
 
 
 def main():
-    print (response(input("What's your email address :")))
-
-
-def response (s):
-    if match := re.search(r"^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$", s):
-        email_address = validators.email(s)
-        return "Valid"
-    else :
-        return "Invalid"
-
+    email_address = input("What's your email address :")
+    try:
+        email_address = validators.email(email_address)
+        print ("Valid")
+    except:
+        print ("Invalid")
+        
 
 if __name__ == "__main__":
     main()
