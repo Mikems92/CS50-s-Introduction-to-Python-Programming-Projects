d4678ae81e3b777a8adac57430c9164b417ec299
Mikems92 <Mikems92@users.noreply.github.com>
2024-02-08 01:31:25 +0100

    automated commit by check50 [check50=True]

--- test_seasons.py
+++ test_seasons.py
@@ -3,7 +3,5 @@ from seasons import check_format, convert
 
 def test_check_format ():
     assert check_format ("2022-01-01") == "2022-01-01"
-    assert check_format ("January, 01 2022") == "Invalid date"
 
-def test_convert ():
-    assert convert ("2024-01-01") == "fifty-four thousand, seven hundred twenty"
+
