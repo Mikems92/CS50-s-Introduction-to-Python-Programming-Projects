5c8243e7bc5227f2e37cf77e583213f8b5209b0b
Mikems92 <Mikems92@users.noreply.github.com>
2024-02-13 22:39:17 +0100

    automated commit by check50 [check50=True]

--- tip.py
+++ tip.py
@@ -1,20 +1,18 @@
 def main():
-    dollars = float(dollars_to_float(input("How much was the meal? ")))
-    percent = float(percent_to_float(input("What percentage would you like to tip? ")))
+    dollars = dollars_to_float(input("How much was the meal? "))
+    percent = percent_to_float(input("What percentage would you like to tip? "))
     tip = dollars * percent
     print(f"Leave ${tip:.2f}")
 
 
 def dollars_to_float(d):
-    # TODO
-    e = float(d.replace("$", ""))
-    return (f"{e: .1f}")
+    float_value = float(d.replace("$", ""))
+    return float_value
 
 
 def percent_to_float(p):
-    # TODO
-    q = float (p.replace("%", ""))
-    return (q/100)
+    percent = float(p.replace("%", ""))/100
+    return percent
 
 
 main()
