9a8efd9c562ca08cd055b278724cf638105687cb
Mikems92 <Mikems92@users.noreply.github.com>
2024-02-15 21:41:10 +0100

    automated commit by check50 [check50=True]

--- extensions.py
+++ extensions.py
@@ -1,5 +1,5 @@
 def main():
-    name = input ("File name :").lower()
+    name = input ("File name :").lower().strip()
     ext = extension(name).replace(".", "")
     if ext in ["gif", "jpg", "jpeg", "png"]:
         print (f"image/{ext}")
