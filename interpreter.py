e7ca3c66e890a04e32b6772c8f4f01985ef91b90
Mikems92 <Mikems92@users.noreply.github.com>
2024-02-17 22:29:53 +0100

    automated commit by check50 [check50=True]

--- interpreter.py
+++ interpreter.py
@@ -1,18 +1,19 @@
 expression = input("Expression :")
 x, y, z = expression.split(" ")
 
-if y == "+":
-    e = int(x) + int(z)
-    print (f"{e:.1f}")
-elif y == "-":
-    e = int(x) - int(z)
-    print (f"{e:.1f}")
-elif y == "*":
-    e = int(x) * int(z)
-    print (f"{e:.1f}")
-elif y == "/":
-    if z == "0":
-        print ("Impossible")
-    else:
-        e = int(x) / int(z)
+match y:
+    case "+":
+        e = int(x) + int(z)
         print (f"{e:.1f}")
+    case "-":
+        e = int(x) - int(z)
+        print (f"{e:.1f}")
+    case "*":
+        e = int(x) * int(z)
+        print (f"{e:.1f}")
+    case "/":
+        if z == "0":
+            print ("Impossible")
+        else:
+            e = int(x) / int(z)
+            print (f"{e:.1f}")
