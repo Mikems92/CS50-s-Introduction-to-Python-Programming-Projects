1750dd2983ff1d628aa2711e0cbb496c332c173c
Mikems92 <Mikems92@users.noreply.github.com>
2024-02-17 22:46:30 +0100

    automated commit by check50 [check50=True]

--- meal.py
+++ meal.py
@@ -1,19 +1,21 @@
 def main():
-    y = convert(input("What time is it ? "))
-    if 7 <= y <= 8 :
-        print("breakfast time")
-    elif 12 <= y <= 13 :
+    question = input ("What time is it?")
+    number_hours = convert(question)
+    if 7.00 <= number_hours <= 8.00:
+        print ("breakfast time")
+    elif 12.00 <= number_hours <= 13.00:
         print("lunch time")
-    elif 18 <= y <= 19 :
-        print("dinner time")
+    elif 18.00 <= number_hours <= 19.00:
+        print ("dinner time")
     else :
         print("")
 
 
 def convert(time):
-    hours , minutes = time.split (":")
-    x = int(hours) + int(minutes)/60
-    return x
+    hours, minutes = time.split(":")
+    minutes = float(minutes) / 60
+    number_hours = float(hours) + minutes
+    return number_hours
 
 
 if __name__ == "__main__":
