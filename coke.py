1775a1aa713867f90a45ff3fc93af36c7a03a365
Mikems92 <Mikems92@users.noreply.github.com>
2024-02-19 01:37:32 +0100

    automated commit by check50 [check50=True]

--- coke.py
+++ coke.py
@@ -1,13 +1,13 @@
 amount_due = int(50)
-print (f"Amount Due:{amount_due}")
+print ("Amount Due:", amount_due)
 while amount_due > 0 :
     insert_coin = int(input("Insert Coin:"))
     if insert_coin == 25 or insert_coin == 10 or insert_coin == 5 :
         amount_due -= insert_coin
         if amount_due <= 0:
-            print (f"Change Owed:{amount_due * (-1)}")
+            print ("Change Owed:", amount_due * (-1))
         elif amount_due > 0 :
-            print (f"Amount Due:{amount_due}")
+            print ("Amount Due:", amount_due)
     else:
         True
 
