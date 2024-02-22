304e401492eb6cff48faccf8c09c427a0cc0646b
Mikems92 <Mikems92@users.noreply.github.com>
2024-02-22 22:39:36 +0100

    automated commit by check50 [check50=True]

--- fuel.py
+++ fuel.py
@@ -1,14 +1,21 @@
-fraction = input("Fraction: ").strip()
-x, y = fraction.split("/")
 
-try:
-    ratio = int(x)*100/int(y)
-    resultat = round (ratio)
-    if resultat <= 1:
-        print ("E")
-    elif resultat >= 99:
-        print ("F")
-    else :
-        print (f"{resultat}%")
-except (ValueError, ZeroDivisionError):
-    ...
+
+while True:
+    fraction = input("Fraction: ").strip()
+    x, y = fraction.split("/")
+    try:
+        ratio = int(x)*100/int(y)
+        resultat = round (ratio)
+        if resultat <= 1:
+            print ("E")
+            break
+        elif 99 <= resultat <= 100:
+            print ("F")
+            break
+        elif 100 <= resultat :
+            continue
+        else :
+            print (f"{resultat}%")
+            break
+    except (ValueError, ZeroDivisionError):
+        continue
