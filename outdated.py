4e5aeae09c1739da84754c57caf714eb2a1ffc2f
Mikems92 <Mikems92@users.noreply.github.com>
2024-02-26 23:16:43 +0100

    automated commit by check50 [check50=True]

--- outdated.py
+++ outdated.py
@@ -15,15 +15,13 @@ months = [
 while True :
     date = input ("Date: ").strip()
     if "/" in date:
-        month, day, year = date.split("/")
-    elif "," in date:
-        new_date = date.replace(", ", " ")
-        month, day, year = new_date.split(" ")
-        if month in months :
-            month = months.index(month) + 1
-    elif " " in date and "," not in date :
-        month, day, year = date.split(" ")
-        True
+        mm, dd, yyyy = date.split("/")
+    elif " " in date:
+        mm, dd, yyyy = date.split(" ")
+        if mm in months:
+            mm = int(months.index(mm)) + 1
+            if "," in dd:
+                dd = dd.replace(",", "")
     try:
         if int(mm) > 12 or int(dd) > 31 :
             True
@@ -34,5 +32,27 @@ while True :
     else:
         True
 
-print (f"{yyyy}-{int(mm):02}-{int(dd):02}")
+print (f"{yyyy}-{int(mm):02}-{int(dd):02}")while True:
+    date = input("Date :").strip()
+    if "/" in date:
+        month, day, year = date.split("/")
+    elif "," in date:
+        new_date = date.replace(", ", " ")
+        month, day, year = new_date.split(" ")
+        if month in months :
+            month = months.index(month) + 1
+    elif " " in date and "," not in date :
+        month, day, year = date.split(" ")
+        False
+    try:
+        if int(month) > 12 or int(day) > 31 :
+            True
+        else:
+            break
+    except ValueError :
+        continue
+    else :
+        False
+
+print (f"{year}-{int(month):02}-{int(day):02}")
 
