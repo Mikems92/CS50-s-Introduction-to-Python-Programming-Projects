6cbb29849433387dff53509b98357c174622bf73
Mikems92 <Mikems92@users.noreply.github.com>
2024-02-26 21:31:24 +0100

    automated commit by check50 [check50=True]

--- outdated.py
+++ outdated.py
@@ -1,39 +1,36 @@
 months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
 ]
-while True:
-    date = input("Date :").strip()
+while True :
+    date = input ("Date: ")
     if "/" in date:
-        month, day, year = date.split("/")
-    elif "," in date:
-        new_date = date.replace(", ", " ")
-        month, day, year = new_date.split(" ")
-        if month in months :
-            month = months.index(month) + 1
-    elif " " in date and "," not in date :
-        month, day, year = date.split(" ")
-        False
+        mm, dd, yyyy = date.split("/")
+    elif " " in date:
+        mm, dd, yyyy = date.split(" ")
+        if mm in months:
+            mm = int(months.index(mm)) + 1
+            if "," in dd:
+                dd = dd.replace(",", "")
     try:
-        if int(month) > 12 or int(day) > 31 :
+        if int(mm) > 12 and int(dd) > 31 :
             True
         else:
             break
-    except ValueError :
+    except ValueError:
         continue
-    else :
-        False
+    else:
+        True
 
-print (f"{year}-{int(month):02}-{int(day):02}")
-
+print (f"{yyyy}-{int(mm):02}-{int(dd):02}")
 
