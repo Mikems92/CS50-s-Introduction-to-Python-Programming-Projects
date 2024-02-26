715e44870a0ae55c04d70a70ff6a6d9bee0f1c6b
Mikems92 <Mikems92@users.noreply.github.com>
2024-02-26 23:13:13 +0100

    automated commit by check50 [check50=True]

--- outdated.py
+++ outdated.py
@@ -15,13 +15,15 @@ months = [
 while True :
     date = input ("Date: ").strip()
     if "/" in date:
-        mm, dd, yyyy = date.split("/")
-    elif " " in date:
-        mm, dd, yyyy = date.split(" ")
-        if mm in months:
-            mm = int(months.index(mm)) + 1
-            if "," in dd:
-                dd = dd.replace(",", "")
+        month, day, year = date.split("/")
+    elif "," in date:
+        new_date = date.replace(", ", " ")
+        month, day, year = new_date.split(" ")
+        if month in months :
+            month = months.index(month) + 1
+    elif " " in date and "," not in date :
+        month, day, year = date.split(" ")
+        True
     try:
         if int(mm) > 12 or int(dd) > 31 :
             True
