8340f3020a8c4d5fde5911d3b3f67dc938ca5909
Mikems92 <Mikems92@users.noreply.github.com>
2024-03-28 22:01:07 +0100

    automated commit by check50 [check50=True]

--- professor.py
+++ professor.py
@@ -43,13 +43,13 @@ def get_level():
 
 def generate_integer(level):
     match level:
-        case "1" :
+        case "f{1}" :
             x = random.randint(0, 9)
             y = random.randint(0, 9)
-        case "2" :
+        case "f{2}" :
             x = random.randint(10, 99)
             y = random.randint(10, 99)
-        case "3" :
+        case "f{3}" :
             x = random.randint(100, 999)
             y = random.randint(100, 999)
     return x, y
